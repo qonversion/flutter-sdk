70ce45ef8b985581badab380afd814153073d6b9
Ilya Virnik <ilya.virnik@gmail.com>
2020-03-01 18:15:06 +0300

    update ios dependency version & package name

--- ios/Classes/QonversionFlutterSdkPlugin.m
+++ ios/Classes/QonversionFlutterSdkPlugin.m
@@ -1,11 +1,11 @@
 #import "QonversionFlutterSdkPlugin.h"
-#if __has_include(<qonversion_flutter_sdk/qonversion_flutter_sdk-Swift.h>)
-#import <qonversion_flutter_sdk/qonversion_flutter_sdk-Swift.h>
+#if __has_include(<qonversion_flutter/qonversion_flutter-Swift.h>)
+#import <qonversion_flutter/qonversion_flutter-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "qonversion_flutter_sdk-Swift.h"
+#import "qonversion_flutter-Swift.h"
 #endif
 
 @implementation QonversionFlutterSdkPlugin
