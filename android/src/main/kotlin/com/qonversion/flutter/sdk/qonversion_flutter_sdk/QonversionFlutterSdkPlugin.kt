898926aea8496b7232769c4fe18e4aa52ff74664
Ilya Virnik <ilya.virnik@gmail.com>
2020-06-20 21:33:18 +0300

    android platform code remastered

--- android/src/main/kotlin/com/qonversion/flutter/sdk/qonversion_flutter_sdk/QonversionFlutterSdkPlugin.kt
+++ android/src/main/kotlin/com/qonversion/flutter/sdk/qonversion_flutter_sdk/QonversionFlutterSdkPlugin.kt
@@ -1,10 +1,8 @@
 package com.qonversion.flutter.sdk.qonversion_flutter_sdk
 
+import android.app.Activity
 import android.app.Application
 import androidx.annotation.NonNull;
-import com.android.billingclient.api.BillingClient
-import com.android.billingclient.api.BillingResult
-import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -14,99 +12,67 @@ import io.flutter.plugin.common.PluginRegistry.Registrar
 import com.qonversion.android.sdk.Qonversion;
 import com.qonversion.android.sdk.QonversionBillingBuilder
 import com.qonversion.android.sdk.QonversionCallback
-import io.flutter.embedding.engine.plugins.activity.ActivityAware
-import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 
 /** QonversionFlutterSdkPlugin */
-class QonversionFlutterSdkPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
-  private lateinit var channel : MethodChannel
-  private var application: Application? = null
+class QonversionFlutterSdkPlugin internal constructor(registrar: Registrar): MethodCallHandler {
+    private val activity: Activity = registrar.activity()
+    private val application: Application = activity.application
 
-
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "qonversion_flutter_sdk")
-    channel.setMethodCallHandler(this);
-  }
-
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
-
-  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    this.application = binding.activity.application
-  }
-
-  override fun onDetachedFromActivityForConfigChanges() {
-    this.application = null
-  }
-
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    this.application = binding.activity.application
-  }
-
-  override fun onDetachedFromActivity() {
-    this.application = null
-  }
-
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "qonversion_flutter_sdk")
-      channel.setMethodCallHandler(QonversionFlutterSdkPlugin())
-    }
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    val args = call.arguments as? Map<String, Any> ?: return result.error("1",
-            "Please provide arguments",
-            "There was no arguments in method call")
-
-    val key = args["key"] as? String
-    val internalUserId = args["userID"] as? String
-
-    when (call.method) {
-      "launchWithKeyCompletion" -> launchWith(key, internalUserId, result)
-      "launchWithKeyUserId" -> launchWith(key, internalUserId, result)
-      "launchWithKeyAutoTrackPurchasesCompletion" -> launchWith(key, internalUserId, result)
-      "addAttributionData" -> result.notImplemented() // since there is no such method in Android SDK
-      else -> result.notImplemented()
-    }
-  }
-
-  private fun launchWith(key: String?, internalUserId: String?, result: Result) {
-    if (key == null) {
-      return result.error("1", "Could not find API key", "Please provide valid API key")
+    companion object {
+        @JvmStatic
+        fun registerWith(registrar: Registrar) {
+            val channel = MethodChannel(registrar.messenger(), "qonversion_flutter_sdk")
+            channel.setMethodCallHandler(QonversionFlutterSdkPlugin(registrar))
+        }
     }
 
-    val billingBuilder = buildBilling()
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        val args = call.arguments as? Map<String, Any> ?: return result.error("1",
+                "Please provide arguments",
+                "There was no arguments in method call")
 
-    val callback = object: QonversionCallback {
-      override fun onSuccess(uid: String) {
-        result.success(uid)
-      }
+        val key = args["key"] as? String
+        val internalUserId = args["userID"] as? String
+        val autoTrackPurchases = args["autoTrackPurchases"] as? Boolean ?: true
 
-      override fun onError(t: Throwable) {
-        result.error("1", t.localizedMessage, t.cause.toString())
-      }
+        when (call.method) {
+            "launchWithKeyCompletion" -> launchWith(key, internalUserId, autoTrackPurchases, result)
+            "launchWithKeyUserId" -> launchWith(key, internalUserId, autoTrackPurchases, result)
+            "launchWithKeyAutoTrackPurchasesCompletion" -> launchWith(key, internalUserId, autoTrackPurchases, result)
+            "addAttributionData" -> result.notImplemented() // since there is no such method in Android SDK
+            else -> result.notImplemented()
+        }
     }
 
-    val app = application ?: return
+    private fun launchWith(key: String?,
+                           internalUserId: String?,
+                           autoTrackPurchases: Boolean,
+                           result: Result) {
+        if (key == null) {
+          return result.error("1", "Could not find API key", "Please provide valid API key")
+        }
 
-    Qonversion.initialize(
-            app,
-            key,
-            internalUserId ?: "",
-            billingBuilder,
-            true,
-            callback
-    )
-  }
+        val billingBuilder = QonversionBillingBuilder()
+                .enablePendingPurchases()
+                .setListener { _, _ ->  }
 
-  private fun buildBilling(): QonversionBillingBuilder {
-    return QonversionBillingBuilder()
-            .enablePendingPurchases()
-            .setChildDirected(BillingClient.ChildDirected.CHILD_DIRECTED)
-            .setUnderAgeOfConsent(BillingClient.UnderAgeOfConsent.UNSPECIFIED)
-            .setListener { _, _ -> }
-  }
+        val callback = object: QonversionCallback {
+            override fun onSuccess(uid: String) {
+                result.success(uid)
+            }
+
+            override fun onError(t: Throwable) {
+                result.error("1", t.localizedMessage, t.cause.toString())
+            }
+        }
+
+        Qonversion.initialize(
+                application,
+                key,
+                internalUserId,
+                billingBuilder,
+                autoTrackPurchases,
+                callback
+        )
+    }
 }
