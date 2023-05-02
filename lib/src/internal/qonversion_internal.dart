f6de9d94c85a09cbc414ffd67fa5a32f17cd8bc1
Surik <action@github.com>
2023-05-02 18:25:19 +0400

    Updated spaces

--- lib/src/internal/qonversion_internal.dart
+++ lib/src/internal/qonversion_internal.dart
@@ -16,10 +16,10 @@ class QonversionInternal implements Qonversion {
   final MethodChannel _channel = MethodChannel('qonversion_plugin');
 
   final _updatedEntitlementsEventChannel =
-      EventChannel('qonversion_flutter_updated_entitlements');
+  EventChannel('qonversion_flutter_updated_entitlements');
 
   final _promoPurchasesEventChannel =
-      EventChannel('qonversion_flutter_promo_purchases');
+  EventChannel('qonversion_flutter_promo_purchases');
 
 
   QonversionInternal(QonversionConfig config) {
@@ -27,9 +27,12 @@ class QonversionInternal implements Qonversion {
 
     final args = {
       Constants.kProjectKey: config.projectKey,
-      Constants.kLaunchMode: StringUtils.capitalize(describeEnum(config.launchMode)),
-      Constants.kEnvironment: StringUtils.capitalize(describeEnum(config.environment)),
-      Constants.kEntitlementsCacheLifetime: StringUtils.capitalize(describeEnum(config.entitlementsCacheLifetime)),
+      Constants.kLaunchMode: StringUtils.capitalize(
+          describeEnum(config.launchMode)),
+      Constants.kEnvironment: StringUtils.capitalize(
+          describeEnum(config.environment)),
+      Constants.kEntitlementsCacheLifetime: StringUtils.capitalize(
+          describeEnum(config.entitlementsCacheLifetime)),
       Constants.kProxyUrl: config.proxyUrl,
       Constants.kKidsMode: config.kidsMode,
     };
@@ -53,7 +56,8 @@ class QonversionInternal implements Qonversion {
       _promoPurchasesEventChannel.receiveBroadcastStream().cast<String>();
 
   @override
-  Future<void> syncHistoricalData() => _channel.invokeMethod(Constants.mSyncHistoricalData);
+  Future<void> syncHistoricalData() =>
+      _channel.invokeMethod(Constants.mSyncHistoricalData);
 
   @override
   Future<void> syncStoreKit2Purchases() async {
@@ -62,225 +66,232 @@ class QonversionInternal implements Qonversion {
     }
   }
 
-    @override
-    Future<Map<String, QEntitlement>> purchase(String productId) async {
-      try {
-        final rawResult = await _channel
-            .invokeMethod(Constants.mPurchase, {Constants.kProductId: productId});
-
-        final result = QMapper.entitlementsFromJson(rawResult);
-        return result;
-      } on PlatformException catch (e) {
-        throw _convertPurchaseException(e);
-      }
-    }
-
-    @override
-    Future<Map<String, QEntitlement>> purchaseProduct(QProduct product) async {
-      try {
-        final rawResult = await _channel.invokeMethod(
-            Constants.mPurchaseProduct,
-            {
-              Constants.kProductId: product.qonversionId,
-              Constants.kOfferingId: product.offeringID
-            }
-        );
-
-        final result = QMapper.entitlementsFromJson(rawResult);
-        return result;
-      } on PlatformException catch (e) {
-        throw _convertPurchaseException(e);
-      }
-    }
-
-    @override
-    Future<Map<String, QEntitlement>?> updatePurchase({
-      required String newProductId,
-      required String oldProductId,
-      QProrationMode? prorationMode,
-    }) async {
-      if (!Platform.isAndroid) {
-        return null;
-      }
-
-      try {
-        final rawResult = await _channel.invokeMethod(Constants.mUpdatePurchase, {
-          Constants.kNewProductId: newProductId,
-          Constants.kOldProductId: oldProductId,
-          Constants.kProrationMode: prorationMode != null ? prorationMode.index : null,
-        });
-        final result = QMapper.entitlementsFromJson(rawResult);
-        return result;
-      } on PlatformException catch (e) {
-        throw _convertPurchaseException(e);
-      }
-    }
-
-    @override
-    Future<Map<String, QEntitlement>?> updatePurchaseWithProduct({
-      required QProduct newProduct,
-      required String oldProductId,
-      QProrationMode? prorationMode,
-    }) async {
-      if (!Platform.isAndroid) {
-        return null;
-      }
-
-      try {
-        final rawResult = await _channel.invokeMethod(Constants.mUpdatePurchaseWithProduct, {
-          Constants.kNewProductId: newProduct.qonversionId,
-          Constants.kOfferingId: newProduct.offeringID,
-          Constants.kOldProductId: oldProductId,
-          Constants.kProrationMode: prorationMode != null ? prorationMode.index : null,
-        });
-        final result = QMapper.entitlementsFromJson(rawResult);
-        return result;
-      } on PlatformException catch (e) {
-        throw _convertPurchaseException(e);
-      }
-    }
-
-    @override
-    Future<Map<String, QProduct>> products() async {
-      final rawResult = await _channel.invokeMethod(Constants.mProducts);
-
-      final result = QMapper.productsFromJson(rawResult);
-      return result;
-    }
-
-    @override
-    Future<QOfferings> offerings() async {
-      final offeringsString = await _channel.invokeMethod<String>(Constants.mOfferings);
-
-      final result = QMapper.offeringsFromJson(offeringsString);
-      return result;
-    }
-
-    @override
-    Future<Map<String, QEligibility>> checkTrialIntroEligibility(List<String> ids) async {
-      final eligibilitiesString = await _channel.invokeMethod<String>(
-          Constants.mCheckTrialIntroEligibility, {"ids": ids});
-
-      final result = QMapper.eligibilityFromJson(eligibilitiesString);
-      return result;
-    }
-
-    @override
-    Future<Map<String, QEntitlement>> checkEntitlements() async {
-      final rawResult = await _channel.invokeMethod(Constants.mCheckEntitlements);
+  @override
+  Future<Map<String, QEntitlement>> purchase(String productId) async {
+    try {
+      final rawResult = await _channel
+          .invokeMethod(Constants.mPurchase, {Constants.kProductId: productId});
 
       final result = QMapper.entitlementsFromJson(rawResult);
       return result;
-    }
-
-    @override
-    Future<Map<String, QEntitlement>> restore() async {
-      final rawResult = await _channel.invokeMethod(Constants.mRestore);
-
-      final result = QMapper.entitlementsFromJson(rawResult);
-      return result;
-    }
-
-    @override
-    Future<void> syncPurchases() async {
-      if (Platform.isAndroid) {
-        return _channel.invokeMethod(Constants.mSyncPurchases);
-      }
-    }
-
-    @override
-    Future<void> identify(String userId) =>
-        _channel.invokeMethod(Constants.mIdentify, {Constants.kUserId: userId});
-
-    @override
-    Future<void> logout() => _channel.invokeMethod(Constants.mLogout);
-
-    @override
-    Future<QUser> userInfo() async {
-      final rawResult = await _channel.invokeMethod(Constants.mUserInfo);
-
-      final result = QMapper.userFromJson(rawResult);
-      if (result == null) {
-        throw new Exception("User deserialization failed");
-      }
-      return result;
-    }
-
-    @override
-    Future<void> attribution(Map<dynamic, dynamic> data, QAttributionProvider provider) {
-      final args = {
-        Constants.kData: data,
-        Constants.kProvider: StringUtils.capitalize(describeEnum(provider)),
-      };
-
-      return _channel.invokeMethod(Constants.mAddAttributionData, args);
-    }
-
-    @override
-    Future<void> setProperty(QUserProperty property, String value) =>
-        _channel.invokeMethod(Constants.mSetDefinedUserProperty, {
-          Constants.kProperty: StringUtils.capitalize(describeEnum(property)),
-          Constants.kValue: value,
-        });
-
-    @override
-    Future<void> setUserProperty(String property, String value) =>
-        _channel.invokeMethod(Constants.mSetCustomUserProperty, {
-          Constants.kProperty: property,
-          Constants.kValue: value,
-        });
-
-    @override
-    Future<void> collectAdvertisingId() async {
-      if (Platform.isIOS) {
-        return _channel.invokeMethod(Constants.mCollectAdvertisingId);
-      }
-    }
-
-    @override
-    Future<void> collectAppleSearchAdsAttribution() async {
-      if (Platform.isIOS) {
-        return _channel.invokeMethod(Constants.mCollectAppleSearchAdsAttribution);
-      }
-    }
-
-    @override
-    Future<void> presentCodeRedemptionSheet() async {
-      if (Platform.isIOS) {
-        return _channel.invokeMethod(Constants.mPresentCodeRedemptionSheet);
-      }
-    }
-
-    @override
-    Future<Map<String, QEntitlement>?> promoPurchase(String productId) async {
-      if (!Platform.isIOS) {
-        return null;
-      }
-
-      try {
-        final rawResult = await _channel.invokeMethod(
-            Constants.mPromoPurchase, {Constants.kProductId: productId});
-        final result = QMapper.entitlementsFromJson(rawResult);
-        return result;
-      } on PlatformException catch (e) {
-        throw _convertPurchaseException(e);
-      }
-    }
-
-    // Private methods
-    Future<void> _storeSdkInfo() =>
-        _channel.invokeMethod(Constants.mStoreSdkInfo, {
-          "version": _sdkVersion,
-          "source": "flutter",
-        });
-
-    static QPurchaseException _convertPurchaseException(PlatformException error) {
-      return QPurchaseException(
-          error.code,
-          error.message ?? "",
-          error.details,
-          isUserCancelled: error.code == "PurchaseCancelledByUser"
-      );
+    } on PlatformException catch (e) {
+      throw _convertPurchaseException(e);
     }
   }
-}
+
+  @override
+  Future<Map<String, QEntitlement>> purchaseProduct(QProduct product) async {
+    try {
+      final rawResult = await _channel.invokeMethod(
+          Constants.mPurchaseProduct,
+          {
+            Constants.kProductId: product.qonversionId,
+            Constants.kOfferingId: product.offeringID
+          }
+      );
+
+      final result = QMapper.entitlementsFromJson(rawResult);
+      return result;
+    } on PlatformException catch (e) {
+      throw _convertPurchaseException(e);
+    }
+  }
+
+  @override
+  Future<Map<String, QEntitlement>?> updatePurchase({
+    required String newProductId,
+    required String oldProductId,
+    QProrationMode? prorationMode,
+  }) async {
+    if (!Platform.isAndroid) {
+      return null;
+    }
+
+    try {
+      final rawResult = await _channel.invokeMethod(Constants.mUpdatePurchase, {
+        Constants.kNewProductId: newProductId,
+        Constants.kOldProductId: oldProductId,
+        Constants.kProrationMode: prorationMode != null
+            ? prorationMode.index
+            : null,
+      });
+      final result = QMapper.entitlementsFromJson(rawResult);
+      return result;
+    } on PlatformException catch (e) {
+      throw _convertPurchaseException(e);
+    }
+  }
+
+  @override
+  Future<Map<String, QEntitlement>?> updatePurchaseWithProduct({
+    required QProduct newProduct,
+    required String oldProductId,
+    QProrationMode? prorationMode,
+  }) async {
+    if (!Platform.isAndroid) {
+      return null;
+    }
+
+    try {
+      final rawResult = await _channel.invokeMethod(
+          Constants.mUpdatePurchaseWithProduct, {
+        Constants.kNewProductId: newProduct.qonversionId,
+        Constants.kOfferingId: newProduct.offeringID,
+        Constants.kOldProductId: oldProductId,
+        Constants.kProrationMode: prorationMode != null
+            ? prorationMode.index
+            : null,
+      });
+      final result = QMapper.entitlementsFromJson(rawResult);
+      return result;
+    } on PlatformException catch (e) {
+      throw _convertPurchaseException(e);
+    }
+  }
+
+  @override
+  Future<Map<String, QProduct>> products() async {
+    final rawResult = await _channel.invokeMethod(Constants.mProducts);
+
+    final result = QMapper.productsFromJson(rawResult);
+    return result;
+  }
+
+  @override
+  Future<QOfferings> offerings() async {
+    final offeringsString = await _channel.invokeMethod<String>(
+        Constants.mOfferings);
+
+    final result = QMapper.offeringsFromJson(offeringsString);
+    return result;
+  }
+
+  @override
+  Future<Map<String, QEligibility>> checkTrialIntroEligibility(
+      List<String> ids) async {
+    final eligibilitiesString = await _channel.invokeMethod<String>(
+        Constants.mCheckTrialIntroEligibility, {"ids": ids});
+
+    final result = QMapper.eligibilityFromJson(eligibilitiesString);
+    return result;
+  }
+
+  @override
+  Future<Map<String, QEntitlement>> checkEntitlements() async {
+    final rawResult = await _channel.invokeMethod(Constants.mCheckEntitlements);
+
+    final result = QMapper.entitlementsFromJson(rawResult);
+    return result;
+  }
+
+  @override
+  Future<Map<String, QEntitlement>> restore() async {
+    final rawResult = await _channel.invokeMethod(Constants.mRestore);
+
+    final result = QMapper.entitlementsFromJson(rawResult);
+    return result;
+  }
+
+  @override
+  Future<void> syncPurchases() async {
+    if (Platform.isAndroid) {
+      return _channel.invokeMethod(Constants.mSyncPurchases);
+    }
+  }
+
+  @override
+  Future<void> identify(String userId) =>
+      _channel.invokeMethod(Constants.mIdentify, {Constants.kUserId: userId});
+
+  @override
+  Future<void> logout() => _channel.invokeMethod(Constants.mLogout);
+
+  @override
+  Future<QUser> userInfo() async {
+    final rawResult = await _channel.invokeMethod(Constants.mUserInfo);
+
+    final result = QMapper.userFromJson(rawResult);
+    if (result == null) {
+      throw new Exception("User deserialization failed");
+    }
+    return result;
+  }
+
+  @override
+  Future<void> attribution(Map<dynamic, dynamic> data,
+      QAttributionProvider provider) {
+    final args = {
+      Constants.kData: data,
+      Constants.kProvider: StringUtils.capitalize(describeEnum(provider)),
+    };
+
+    return _channel.invokeMethod(Constants.mAddAttributionData, args);
+  }
+
+  @override
+  Future<void> setProperty(QUserProperty property, String value) =>
+      _channel.invokeMethod(Constants.mSetDefinedUserProperty, {
+        Constants.kProperty: StringUtils.capitalize(describeEnum(property)),
+        Constants.kValue: value,
+      });
+
+  @override
+  Future<void> setUserProperty(String property, String value) =>
+      _channel.invokeMethod(Constants.mSetCustomUserProperty, {
+        Constants.kProperty: property,
+        Constants.kValue: value,
+      });
+
+  @override
+  Future<void> collectAdvertisingId() async {
+    if (Platform.isIOS) {
+      return _channel.invokeMethod(Constants.mCollectAdvertisingId);
+    }
+  }
+
+  @override
+  Future<void> collectAppleSearchAdsAttribution() async {
+    if (Platform.isIOS) {
+      return _channel.invokeMethod(Constants.mCollectAppleSearchAdsAttribution);
+    }
+  }
+
+  @override
+  Future<void> presentCodeRedemptionSheet() async {
+    if (Platform.isIOS) {
+      return _channel.invokeMethod(Constants.mPresentCodeRedemptionSheet);
+    }
+  }
+
+  @override
+  Future<Map<String, QEntitlement>?> promoPurchase(String productId) async {
+    if (!Platform.isIOS) {
+      return null;
+    }
+
+    try {
+      final rawResult = await _channel.invokeMethod(
+          Constants.mPromoPurchase, {Constants.kProductId: productId});
+      final result = QMapper.entitlementsFromJson(rawResult);
+      return result;
+    } on PlatformException catch (e) {
+      throw _convertPurchaseException(e);
+    }
+  }
+
+  // Private methods
+  Future<void> _storeSdkInfo() =>
+      _channel.invokeMethod(Constants.mStoreSdkInfo, {
+        "version": _sdkVersion,
+        "source": "flutter",
+      });
+
+  static QPurchaseException _convertPurchaseException(PlatformException error) {
+    return QPurchaseException(
+        error.code,
+        error.message ?? "",
+        error.details,
+        isUserCancelled: error.code == "PurchaseCancelledByUser"
+    );
+  }
+}}
